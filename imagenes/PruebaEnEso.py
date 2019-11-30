cce84335a75b2993a1b2fc45e73cb3df21d6a93d
AndresRey3738 <56320332+AndresRey3738@users.noreply.github.com>
2019-11-30 15:37:48 -0500

    Update PruebaEnEso.py

--- imagenes/PruebaEnEso.py
+++ imagenes/PruebaEnEso.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-for count in range(10,47):
-    
-	html_doc = requests.get('https://www.eso.org/public/images/potw19'+repr(count)+'a/', headers = {'Accept-Encoding':'identity'})
+for count in range(1,47):
+    if(count<10):
+        html_doc = requests.get('https://www.eso.org/public/images/potw190'+repr(count)+'a/', headers = {'Accept-Encoding':'identity'})
+        soup = BeautifulSoup(html_doc.content, 'html.parser')
 
-	#print(html_doc.content)
+        print(soup.img.get("src"))
 
-	soup = BeautifulSoup(html_doc.content, 'html.parser')
+        nombre_local_imagen = "ima"+repr(count)+".jpg"
+        imagen = requests.get(soup.img.get("src")).content
+        with open(nombre_local_imagen, 'wb') as handler:
+            handler.write(imagen)
+    else:   
+        html_doc = requests.get('https://www.eso.org/public/images/potw19'+repr(count)+'a/', headers = {'Accept-Encoding':'identity'})
 
-	print(soup.img.get("src"))
 
-	nombre_local_imagen = "ima"+repr(count)+".jpg"
-	imagen = requests.get(soup.img.get("src")).content
-	with open(nombre_local_imagen, 'wb') as handler:
-		handler.write(imagen)
+        #print(html_doc.content)
+
+        soup = BeautifulSoup(html_doc.content, 'html.parser')
+
+        print(soup.img.get("src"))
+
+        nombre_local_imagen = "ima"+repr(count)+".jpg"
+        imagen = requests.get(soup.img.get("src")).content
+        with open(nombre_local_imagen, 'wb') as handler:
+            handler.write(imagen)
